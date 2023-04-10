e51780d3dce1874bbdac4ddd5d1d11942b2ffeec
SangeethArulraj <sangeeth@swivelgroup.com.au>
2023-04-10 16:45:37 +0530

    minor change

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,9 @@ import config from "config";
 import connect from "./utils/connect";
 import log from "./utils/logger";
 import createServer from "./utils/server";
-// import swaggerUi from "swagger-ui-express";
-
-// const swaggerDocs = require("../swagger.json");
 
 const app = createServer();
 const port = process.env.PORT || config.get<number>("port");
-
-// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.get("/", (req, res) => {
   res.send("home page");
