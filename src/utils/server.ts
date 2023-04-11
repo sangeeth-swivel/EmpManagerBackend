6733fbe791500b361cdb032e32936588bb44307f
SangeethArulraj <sangeeth@swivelgroup.com.au>
2023-04-11 19:05:38 +0530

    server change

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,14 +1,11 @@
 import express from "express";
 import cors from "cors";
 import router from "../routes";
-var bodyParser = require("body-parser");
 
 const createServer = () => {
   const app = express();
-  // app.use(express.json());
-  // app.use(express.urlencoded({ extended: true }));
-  app.use(bodyParser.urlencoded({ extended: false }));// parse application/x-www-form-urlencoded
-  app.use(bodyParser.json());  // parse application/json
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
   app.use(cors());
   app.use("/employee", router);
 
